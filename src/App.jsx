12ab167fcc8b867807d16f7c5630dbc0146654e6
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2023-07-11 17:41:27 -0300

    context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { GlobalProvider } from "./context/GlobalState";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import TransactionForm from "./components/TransactionForm";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <GlobalProvider>
+      <Header />
+      <Balance />
+      <TransactionForm />
+      <h1>Hello World</h1>
+    </GlobalProvider>
+  );
 }
 
 export default App;
---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react-refresh/only-export-components
+export default (state, action) => {
+  switch (action.type) {
+    case "ADD_TRANSACTION":
+      return state;
+    default:
+      return state;
+  }
+};
