6951ecacb75213c327b7607ccae02faaf4913d51
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2023-07-12 01:21:22 -0300

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Fazt--React_Aplicacion_para_administrar_gastos_e_ingresos/",
+});
